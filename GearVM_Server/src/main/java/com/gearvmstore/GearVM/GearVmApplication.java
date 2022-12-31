1d1d9dbae70987a4bce3d3fd23131ca0f0396074
Hoàng Tuấn <80930248+hoangtuandrive@users.noreply.github.com>
2022-12-31 18:46:11 +0700

    Enable CORS

--- GearVM_Server/src/main/java/com/gearvmstore/GearVM/GearVmApplication.java
+++ GearVM_Server/src/main/java/com/gearvmstore/GearVM/GearVmApplication.java
@@ -2,12 +2,19 @@ package com.gearvmstore.GearVM;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @SpringBootApplication
 public class GearVmApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GearVmApplication.class, args);
+        SpringApplication.run(GearVmApplication.class, args);
 	}
-
 }
---
+++ GearVM_Server/src/main/java/com/gearvmstore/GearVM/configuration/GlobalCorsConfiguration.java
@@ -0,0 +1,31 @@
+package com.gearvmstore.GearVM.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+@Configuration
+public class GlobalCorsConfiguration {
+    public GlobalCorsConfiguration() {
+        super();
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        // Don't do this in production, use a proper list  of allowed origins
+        config.setAllowedOrigins((List.of("http://localhost:3000")));
+        config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
