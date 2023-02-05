b0fb033b33e1c114c3360b5d00131580c4bf5f93
Hoàng Tuấn <80930248+hoangtuandrive@users.noreply.github.com>
2023-02-05 21:30:43 +0700

    Async

---
+++ GearVM_Server/src/main/java/com/gearvmstore/GearVM/configuration/SpringAsyncConfig.java
@@ -0,0 +1,32 @@
+package com.gearvmstore.GearVM.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class SpringAsyncConfig implements AsyncConfigurer {
+    @Bean(name = "threadPoolTaskExecutor")
+    public Executor threadPoolTaskExecutor() {
+        return new ThreadPoolTaskExecutor();
+    }
+
+    @Async("threadPoolTaskExecutor")
+    public void asyncMethodWithConfiguredExecutor() {
+        System.out.println("Execute method with configured executor - "
+                + Thread.currentThread().getName());
+    }
+
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
+        threadPoolTaskExecutor.initialize();
+        return threadPoolTaskExecutor;
+    }
+}
