fd9852b253ca0f490bc1abe6e952f7534d2bc62b
Tran Hoang Long <tranhoanglong720@gmail.com>
2023-05-28 02:09:18 +0700

    fix key

--- GearVM_WebClient/src/aws.js
+++ GearVM_WebClient/src/aws.js
@@ -1,8 +1,6 @@
 import AWS from "aws-sdk";
 
 AWS.config.update({
-  // accessKeyId: "AKIA3XURV5V43S6M4JER",
-  // secretAccessKey: "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",
   accessKeyId: process.env.accessKeyId,
   secretAccessKey: process.env.secretAccessKey,
   region: "ap-southeast-1",
--- GearVM_WebClient/src/chatgpt.js
+++ GearVM_WebClient/src/chatgpt.js
@@ -1,2 +1,1 @@
-// export const chatgpt = "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";
 export const chatgpt = process.env.chatgpt;
