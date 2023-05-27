9cc189a2120a28aaa67a8c738b2b5e6dc27cb2f9
Tran Hoang Long <tranhoanglong720@gmail.com>
2023-05-28 01:52:28 +0700

    vercel

--- GearVM_WebClient/src/chatgpt.js
+++ GearVM_WebClient/src/chatgpt.js
@@ -1,2 +1,2 @@
 // export const chatgpt = "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";
-export const chatgpt = "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";
+export const chatgpt = process.env.chatgpt;
