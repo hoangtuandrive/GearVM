096e1e303db3007fecfab434ec720150cf9b2a84
Tran Hoang Long <tranhoanglong720@gmail.com>
2023-05-28 02:33:32 +0700

    log

--- GearVM_WebClient/src/component/chatBox/ChatBox.jsx
+++ GearVM_WebClient/src/component/chatBox/ChatBox.jsx
@@ -107,7 +107,7 @@ const ChatBox = ({ name }) => {
     // Get the request body set up with the model we plan to use
     // and the messages which we formatted above. We add a system message in the front to'
     // determine how we want chatGPT to act.
-    const apiRequestBody = {  
+    const apiRequestBody = {
       model: "gpt-3.5-turbo",
       messages: [
         systemMessage, // The system message DEFINES the logic of our chatGPT
@@ -115,6 +115,7 @@ const ChatBox = ({ name }) => {
       ],
     };
 
+    console.log("ChatGPt", chatgpt);
     await fetch("https://api.openai.com/v1/chat/completions", {
       method: "POST",
       headers: {
